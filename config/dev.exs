8b0d2e19aa6f680882ba800c584c09522f2b4358
Diego Linhares <diegomlinhares@gmail.com>
2022-10-21 17:11:10 -0300

    Fix typos

--- config/dev.exs
+++ config/dev.exs
@@ -28,9 +28,9 @@ config :cd_gigalixir, CdGigalixirWeb.Endpoint,
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
     npx: [
-      "tailwidcss",
+      "tailwindcss",
       "--input=css/app.css",
-      "--output=../priv/static/assets",
+      "--output=../priv/static/assets/app.css",
       "--postcss",
       "--watch",
       cd: Path.expand("../assets", __DIR__)

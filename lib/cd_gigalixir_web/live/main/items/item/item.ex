fd29605eec2393a5bc003c07a773028d9198fd9a
Diego Linhares <diegomlinhares@gmail.com>
2022-10-22 00:20:10 -0300

    Add components

---
+++ lib/cd_gigalixir_web/live/main/items/item/item.ex
@@ -0,0 +1,3 @@
+defmodule CdGigalixirWeb.Main.Items.Item do
+  use CdGigalixirWeb, :live_component
+end
---
+++ lib/cd_gigalixir_web/live/main/items/items.ex
@@ -0,0 +1,5 @@
+defmodule CdGigalixirWeb.Main.Items do
+  use CdGigalixirWeb, :live_component
+
+  alias CdGigalixirWeb.Main.Items.Item
+end

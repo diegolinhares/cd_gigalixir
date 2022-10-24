0e749e1644fdfcae518b9d46025a6319f0bfbf1f
Diego Linhares <diegomlinhares@gmail.com>
2022-10-24 19:47:17 -0300

    Create modal component

--- lib/cd_gigalixir_web/live/admin/products/product_live.ex
+++ lib/cd_gigalixir_web/live/admin/products/product_live.ex
@@ -1,6 +1,7 @@
 defmodule CdGigalixirWeb.Admin.ProductLive do
   use CdGigalixirWeb, :live_view
   alias CdGigalixir.Products
+  alias CdGigalixirWeb.Admin.Products.Form
 
   def mount(_assings, _session, socket) do
     products = Products.list_products()

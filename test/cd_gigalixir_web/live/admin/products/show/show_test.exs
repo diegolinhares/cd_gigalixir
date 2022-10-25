680af5364d65a5f73bba700ad2803a38077913b7
Diego Linhares <diegomlinhares@gmail.com>
2022-10-25 12:37:29 -0300

    Add show product

---
+++ test/cd_gigalixir_web/live/admin/products/show/show_test.exs
@@ -0,0 +1,16 @@
+defmodule CdGigalixirWeb.Admin.Show.ShowTest do
+  use CdGigalixirWeb.ConnCase
+  import CdGigalixir.Factory
+  import Phoenix.LiveViewTest
+
+  test "load page", %{conn: conn} do
+    product = insert(:product)
+
+    {:ok, view, _html} = live(conn, Routes.admin_product_show_path(conn, :show, product))
+
+    assert has_element?(view, "[data-role=product-description]", product.description)
+    assert has_element?(view, "[data-role=product-name]", product.name)
+    assert has_element?(view, "[data-role=product-size]", product.size)
+    assert has_element?(view, "[data-role=product-price]", product.price |> Money.to_string())
+  end
+end

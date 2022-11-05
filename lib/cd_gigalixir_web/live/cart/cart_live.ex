27ffe797ddc45e0df56fdb1ceb2222087e5bf7ba
Diego Linhares <diegomlinhares@gmail.com>
2022-11-04 21:30:33 -0300

    Add cart live

--- lib/cd_gigalixir_web/live/cart/cart_live.ex
+++ lib/cd_gigalixir_web/live/cart/cart_live.ex
@@ -1,10 +1,20 @@
 defmodule CdGigalixirWeb.CartLive do
   use CdGigalixirWeb, :live_view
 
+  alias CdGigalixir.Carts
   alias CdGigalixirWeb.CartLive.CartDetail
   alias CdGigalixirWeb.CartLive.EmptyCart
 
   def mount(_assigns, _session, socket) do
+    cart_id = socket.assigns.cart_id
+    cart = Carts.get(cart_id)
+
+    IO.inspect(cart)
+
+    socket =
+      socket
+      |> assign(cart: cart)
+
     {:ok, socket}
   end
 end
--- lib/cd_gigalixir_web/live/main/items/item/item.ex
+++ lib/cd_gigalixir_web/live/main/items/item/item.ex
@@ -1,5 +1,24 @@
 defmodule CdGigalixirWeb.Main.Items.Item do
   use CdGigalixirWeb, :live_component
 
+  alias CdGigalixir.Carts
   alias CdGigalixir.Products
+
+  def handle_event("add", _, socket) do
+    product = socket.assigns.product
+    cart_id = socket.assigns.cart_id
+
+    add_product_to_cart(cart_id, product)
+
+    socket =
+      socket
+      |> put_flash(:info, "Item added to cart")
+      |> push_redirect(to: "/")
+
+    {:noreply, socket}
+  end
+
+  defp add_product_to_cart(cart_id, product) do
+    Carts.add(cart_id, product)
+  end
 end

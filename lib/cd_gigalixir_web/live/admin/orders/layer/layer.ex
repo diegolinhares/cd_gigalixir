e7936a62cea1767f69fc58a6a804cffe7d6e7e12
Diego Linhares <diegomlinhares@gmail.com>
2022-11-05 22:23:40 -0300

    Add layers

---
+++ lib/cd_gigalixir_web/live/admin/orders/layer/layer.ex
@@ -0,0 +1,3 @@
+defmodule CdGigalixirWeb.Admin.OrderLive.Layer do
+  use CdGigalixirWeb, :live_component
+end
--- lib/cd_gigalixir_web/live/admin/orders/order_live.ex
+++ lib/cd_gigalixir_web/live/admin/orders/order_live.ex
@@ -1,6 +1,7 @@
 defmodule CdGigalixirWeb.Admin.OrderLive do
   use CdGigalixirWeb, :live_view
 
+  alias CdGigalixirWeb.Admin.OrderLive.Layer
   alias CdGigalixirWeb.Admin.OrderLive.SideMenu
 
   def mount(_assings, _session, socket) do

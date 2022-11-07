346f057a94194ad39b2f9dcfba9b1a947d328f4f
Diego Linhares <diegomlinhares@gmail.com>
2022-11-07 11:14:19 -0300

    Add user status (#44)
    * Add user status
    
    * Add tests

---
+++ lib/cd_gigalixir_web/live/customer/order/status/status.ex
@@ -0,0 +1,39 @@
+defmodule CdGigalixirWeb.Customer.OrderLive.Status do
+  use CdGigalixirWeb, :live_view
+
+  alias CdGigalixir.Orders
+
+  def mount(_assings, _session, socket) do
+    {:ok, socket}
+  end
+
+  def handle_params(%{"id" => id}, _uri, socket) do
+    if connected?(socket) do
+      Orders.subscribe_update_order(id)
+    end
+
+    current_user = socket.assigns.current_user
+    order = Orders.get_order_by_id_and_customer_id(id, current_user.id)
+    status_list = Orders.get_status_list()
+    current_status = get_current_status(order.status)
+
+    socket =
+      socket
+      |> assign(order: order)
+      |> assign(status_list: status_list)
+      |> assign(current_status: current_status)
+
+    {:noreply, socket}
+  end
+
+  def handle_info({:update_order, order}, socket) do
+    current_status = get_current_status(order.status)
+    {:noreply, socket |> assign(current_status: current_status)}
+  end
+
+  defp get_current_status(current_status) do
+    Orders.get_status_list()
+    |> Enum.find(fn {status, _index} -> status == current_status end)
+    |> then(fn {_, value} -> value end)
+  end
+end
--- test/cd_gigalixir_web/live/customer/order/order_live_test.exs
+++ test/cd_gigalixir_web/live/customer/order/order_live_test.exs
@@ -1,8 +1,11 @@
-defmodule CdGigalixirWeb.Customer.Orders.OrderLiveTest do
+defmodule CdGigalixirWeb.Customer.Order.OrderLiveTest do
   use CdGigalixirWeb.ConnCase
 
   import CdGigalixir.Factory
+  import Phoenix.HTML.Form, only: [humanize: 1]
   import Phoenix.LiveViewTest
+
+  alias CdGigalixir.Orders
 
   describe "order is loaded" do
     setup :register_and_log_in_user
@@ -31,6 +34,22 @@ defmodule CdGigalixirWeb.Customer.Orders.OrderLiveTest do
                view,
                "[data-role=date][data-id=#{order.id}]",
                order.updated_at |> NaiveDateTime.to_string()
+             )
+    end
+
+    test "change card to another place", %{conn: conn, user: user} do
+      order = insert(:order, user: user)
+      {:ok, view, _html} = live(conn, Routes.customer_order_path(conn, :index))
+
+      assert has_element?(view, "[data-role=status][data-id=#{order.id}]", humanize(order.status))
+
+      {:ok, new_order} = Orders.update_order_status(order.id, "NOT_STARTED", "RECEIVED")
+      send(view.pid, {:update_order_user_row, new_order})
+
+      assert has_element?(
+               view,
+               "[data-role=status][data-id=#{new_order.id}]",
+               humanize(new_order.status)
              )
     end
   end
